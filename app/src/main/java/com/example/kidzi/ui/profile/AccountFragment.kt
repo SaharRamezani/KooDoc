34b664996d51ef7e2c935f90bb24a73f93b751c3
SaharRamezani <saharramezanijolfaie@gmail.com>
2025-05-18 01:43:35 +0200

    fragment_profile is deleted and the connection of the account button is right now.
    Fragment_rules must be completed.

--- app/src/main/java/com/example/kidzi/ui/profile/AccountFragment.kt
+++ app/src/main/java/com/example/kidzi/ui/profile/AccountFragment.kt
@@ -5,19 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.kidzi.R
-import com.example.kidzi.databinding.FragmentProfileBinding
+import com.example.kidzi.databinding.FragmentAccountBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class ProfileFragment : Fragment() {
+class AccountFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val binding = FragmentProfileBinding.inflate(inflater)
+        val binding = FragmentAccountBinding.inflate(inflater)
 
 
         return binding.root
